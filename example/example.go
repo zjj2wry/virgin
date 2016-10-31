60c96322a104cb26ef9d1711bf2a4ec790a19824
baizhi <baizhi@jiayukang.com>
2016-10-31 13:59:21 +0800

    modified:   example.go 	modified:   ../route.go 	modified:   ../virgin.go

--- example/example.go
+++ example/example.go
@@ -7,12 +7,20 @@ import (
 )
 
 func test(ctx *virgin.Context) {
-	fmt.Fprintf(ctx.Response, "Request Method:%s", ctx.Request.URL.PATH)
+	fmt.Fprintf(ctx.Response, "Request Method:%s", ctx.Request.Method)
 }
 
 func main() {
 	v := virgin.NewVirgin()
 	v.AddRoute("GET", "/a", test)
+	v.AddRoute("GET", "/ab", test)
+	v.AddRoute("GET", "/ad/:a", test)
+	v.AddRoute("GET", "/av/", test)
+	v.AddRoute("GET", "/adf", test)
+	v.AddRoute("GET", "/asd", test)
+	v.AddRoute("GET", "/b", test)
+	v.AddRoute("GET", "/", test)
+	v.AddRoute("POST", "/dsf", test)
 
 	log.Fatal(v.Listen(":8080"))
 }
