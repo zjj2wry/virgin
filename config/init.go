dc324e76bdd0f182d665d4e456050e3ac5f660b1
白芷 <baizhi@jiayukang.com>
2016-11-11 14:57:53 +0800

    modified:   config/init.go

--- config/init.go
+++ config/init.go
@@ -1,33 +1,17 @@
 package config
 
 import (
-	"config.1kf.com/server/base_go/log"
-	"config.1kf.com/server/base_go/rpc/order"
-	"config.1kf.com/server/base_go/rpc/user"
-)
+	"log"
 
-/***
-定时任务控制器
-*/
+)
 
 var Conf *Config
 
-var (
-	// 订单RPC
-	OrderRPC *order.OrderRPCClient
-	//用户RPC
-	UserPRC *user.UserRPCClient
-)
 
 func init() {
 	Conf = &Config{}
 	Conf.InitConfig("conf/app.conf")
-	log.Info(Conf)
+	log.Println(Conf)
 
-	log.Info("orderrpc = ", Conf.GetString("orderrpc"))
-	OrderRPC = order.NewOrderRPCClient(Conf.GetString("orderrpc"))
-
-	UserPRCURL := Conf.GetString("UserPRCURL")
-	log.Info("userrpc:", UserPRCURL)
-	UserPRC = user.NewUserRPCClient(UserPRCURL)
+	log.Println("orderrpc = ", Conf.GetString("orderrpc"))
 }
